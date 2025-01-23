e9a6bcc0c3f64cd6d8007bb2dee63127e15d0208
AceOfKades <knelson17@cub.uca.edu>
2025-01-22 20:44:24 -0600

    Program functional! Only console test failed 0.005, does not return penny. pursetoSring still requires some prettying up, but denominations are being added successfully!

--- Lab1/src/Register.java
+++ Lab1/src/Register.java
@@ -8,21 +8,33 @@ import java.util.*;
 //public static void main - where the magic happens.
 
 public class Register {
-    Denomination fiftyBill = new Denomination("Fifty-Note", 50.00, "Bill", ""),     //Fifty Dollar Bill
-                 twentyBill = new Denomination("Twenty-Note", 20.00, "Bill", ""),   //Twenty Dollar Bill
-                 tenBill = new Denomination("Ten-Note", 10.00, "Bill", ""),         //Ten Dollar Bill
-                 fiveBill = new Denomination("Five-Note", 5.00, "Bill", ""),        //Five Dollar Bill
-                 oneBill = new Denomination("One-Note", 1.00, "Bill", ""),          //One Dollar Bill
-                 quarter = new Denomination("Quarter", 0.25, "Coin", ""),           //Quarter
-                 dime = new Denomination("Dime", 0.10, "Coin", ""),                 //Dime
-                 nickel = new Denomination("Nickel", 0.05, "Coin", ""),             //Nickel
-                 penny = new Denomination("Penny", 0.01, "Coin", "");               //Penny
 
-    static Purse makeChange(double amt)
+    public static Purse makeChange(double amt)
     {
-        Purse purse = new Purse(); //make this a thing.
+        List<Denomination> denominations = List.of(                                            //List of all denominations
+                new Denomination("Hundred-Dollar", 100.00, "Bill", ""),          //Hundred Dollar Bill
+                new Denomination("Fifty-Dollar", 50.00, "Bill", ""),             //Fifty Dollar Bill
+                new Denomination("Twenty-Dollar", 20.00, "Bill", ""),            //Twenty Dollar Bill
+                new Denomination("Ten-Dollar", 10.00, "Bill", ""),               //Ten Dollar Bill
+                new Denomination("Five-Dollar", 5.00, "Bill", ""),               //Five Dollar Bill
+                new Denomination("One-Dollar", 1.00, "Bill", ""),                //One Dollar Bill
+                new Denomination("Quarter", 0.25, "Coin", ""),            //Quarter
+                new Denomination("Dime", 0.10, "Coin", ""),               //Dime
+                new Denomination("Nickel", 0.05, "Coin", ""),             //Nickel
+                new Denomination("Penny", 0.01, "Coin", "")               //Penny
+        );
 
-        //do things here
+        Purse purse = new Purse();
+
+        for (Denomination list : denominations) {
+            int count = (int)(amt / list.amt()); //type cast the result of dividing amt by the value of the current bill to integer, to know how many of that bill can be derived from amt.
+
+            if(count > 0) //if a number of bills can be derived from amount, add to purse and decrement amt.
+            {
+                purse.add(list, count); // add the number of the current denomination to purse
+                amt -= count*list.amt(); //subtract the number of bills * the value of that bill from amt, then continue iterating.
+            }
+        }
 
         return purse;
     }
@@ -42,6 +54,7 @@ public class Register {
         }
 
         Purse purse = makeChange(amt);
+
         System.out.println(purse.pursetoString());
     }
 
